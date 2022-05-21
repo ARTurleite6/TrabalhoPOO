55435a7673bb5364f11c4d732a9b5447d31fa490
ARTurleite6 <arturleite164@gmail.com>
2022-05-21 01:19:14 +0100

    TEstes progarama

---
+++ Trabalho/src/main/java/smart_houses/Testes/EstadoProgramaTest.java
@@ -0,0 +1,28 @@
+package smart_houses.Testes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EstadoProgramaTest {
+
+    @Test
+    void getCasaMaisGastadora() {
+    }
+
+    @Test
+    void maiorConsumidorPeriodo() {
+    }
+
+    @Test
+    void testMaiorConsumidorPeriodo() {
+    }
+
+    @Test
+    void getFornecedorMaiorFaturacao() {
+    }
+
+    @Test
+    void podiumDeviceMaisUsado() {
+    }
+}
